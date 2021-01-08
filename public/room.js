e6d7cb0a0c99da1a3e1c1c00d416ec34a9503378
Lauren McCarthy <laurenleemccarthy@gmail.com>
2021-01-07 18:04:42 -0800

    ok

--- public/room.js
+++ public/room.js
@@ -4,59 +4,89 @@ firebase.auth().onAuthStateChanged(function(user) { });
 db = firebase.firestore(app);
 
 const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id');
-console.log(id);
-$('h1').text(id);
+const roomid = Number(urlParams.get('id'));
+$('h2').text('Breakout Room '+roomid);
 
 $('#generate').on('click', generate);
 
-db.collection('people').where("room", "==", id)
+db.collection('people').where("room", "==", roomid)
   .get()
   .then(function(snapshot) {
     snapshot.forEach(function(doc) {
       console.log(doc.id, " => ", doc.data());
       displayPerson(doc.data());
     });
+    displayPerson({});
   });
 
 function displayPerson(data) {
-  let elm = $('<div class="person"><input type="text" class="name" value="'+data.name+'" data-id="'+data.id+'"><input type="checkbox" class="present" data-id="'+data.id+'"></div>');
-  $('#people').append(elm);
 
-  if (data.host) {
-    elm.addClass('host');
+  if (data.facilitator) {
+    $('#facilitator').val(data.name);
+    $('#facilitator').data('id', data.id);
+    $('#facilitator').data('facilitator', true);
+    $('#facilitator').on('change', nameAndCheckBlank);
+  } else {
+    let elm = $('<div class="person"><input type="text" class="name participant" ></div>');
+    let dataid = data.id ? data.id : makeid();
+    elm.find('input').attr('data-id', dataid);
+    if (data.name) elm.find('input').val(data.name);
+  
+    $('#people').append(elm);
+    elm.find('input').focus();
+
+    elm.find('input').on('change', nameAndCheckBlank);
+    elm.find('input').bind('keypress', function(event) {
+      if(event.which === 13) {
+        $(this).parent().next().find('input').focus();
+      }
+    });
   }
-  if (data.present) {
-    elm.find('input').prop('checked', true);
-  }
-  elm.find('input[type="text"]').on('change', rename);
-  elm.find('input[type="checkbox"]').on('change', attendance);
 }
 
-function rename(e) {
-  $('#assignments').hide();
-  let id = $(e.target).data('id');
-  let newname = $(e.target).val();
-  console.log(id, newname);
-  db.collection('people').doc(id).update({name: newname});
+function checkBlank() {
+  let blank = false;
+  $('.participant').each(function(i, obj) {
+    if ($(obj).val().length === 0) {
+      blank = true;
+    }
+  });
+  if (!blank) {
+    displayPerson({});
+  }
 }
 
-function attendance(e) {
+function nameAndCheckBlank(e) {
   $('#assignments').hide();
-  let id = $(e.target).data('id');
-  let checked = $(e.target).prop('checked');
-  db.collection('people').doc(id).update({present: checked});
+  let p = {
+    name: $(e.target).val(),
+    id: $(e.target).data('id'),
+    facilitator: Boolean($(e.target).data('facilitator')),
+    room: roomid
+  };
+
+  console.log(p);
+  if (p.name) {
+    db.collection('people').doc(p.id).set(p);
+  } else {
+    db.collection('people').doc(p.id).delete();
+  }
+  checkBlank();
 }
 
 function generate() {
+  if (!$('#facilitator').val()) {
+    alert('Please enter your name in the facilitator box!') 
+    return false;
+  }
+
   $('#assignments').show();
   let people = [];
-  let elts = $('.person');
-  $('.person').each(function(i, obj) {
-    if ($(obj).find('.present').prop('checked')) {
-      if (elts.length % 2 == 0 || !$(obj).hasClass('host')) {
-        people.push($(obj).find('.name').val())
-      }
+  let elts = $('input').filter(function() { return this.value.length !== 0; });
+  console.log(elts)
+  elts.each(function(i, obj) {
+    if (elts.length % 2 == 0 || !$(obj).data('facilitator')) {
+      people.push($(obj).val())
     }
   });
   people = shuffle(people);
@@ -78,8 +108,8 @@ function displayAssignments(people) {
     let texta = 'Please hold the following name in your mind. You will be instructed what to do with it shortly: ' + a;
     let b = people[p+1];
     let textb = 'Please hold the following name in your mind. You will be instructed what to do with it shortly: ' + b;
-    $('#messages').append('<div id="clip'+p+'" class="message"><span class="instruct"><b>'+a+'</b><br>'+textb+'<img src="clip.png"></span> <span class="copied">copied!</span></div>');
-    $('#messages').append('<div id="clip'+(p+1)+'" class="message"><span class="instruct"><b>'+b+'</b><br>'+texta+'<img src="clip.png"></span> <span class="copied">copied!</span></div>');
+    $('#messages').append('<div id="clip'+p+'" class="message"><span class="instruct"><b>Send to '+a+':</b><br>'+textb+'<img src="clip.png" class="clipicon"></span> <span class="copied">copied!</span></div>');
+    $('#messages').append('<div id="clip'+(p+1)+'" class="message"><span class="instruct"><b>Send to '+b+':</b><br>'+texta+'<img src="clip.png" class="clipicon"></span> <span class="copied">copied!</span></div>');
     $('#clip'+p).click(function() { copyToClipboard($(this), textb); });
     $('#clip'+(p+1)).click(function() { copyToClipboard($(this), texta); });
   }
@@ -98,4 +128,15 @@ function copyToClipboard(elem, text) {
   $temp.val(text).select();
   document.execCommand('copy');
   $temp.remove();
+}
+
+function makeid() {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < 6; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  result += Date.now();
+  return result;
 }
